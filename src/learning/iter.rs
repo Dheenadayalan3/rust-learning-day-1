5722015d89196037dfd9f843a6466ba367a1d122
Dheenadayalan3 <dheenadayalannagarajan@gmail.com>
2025-11-29 16:35:53 +0530

    .next() use

--- src/learning/iter.rs
+++ src/learning/iter.rs
@@ -4,7 +4,7 @@ pub fn iteration(){
 
     let mut vector1_iter = vector1.iter_mut();
 
-    while let Some(value) = vector1_iter.next(){    // .next() gives Option<&mut i32>
+    while let Some(value) = vector1_iter.next(){    //.next() gives Option<&mut i32>
         *value = *value + 1;
     }
 
